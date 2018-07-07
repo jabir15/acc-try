5db5577e6a53a361c58d1e88b98c14bacde74f41
jabir15 <jabir.hussain.aec@gmail.com>
2018-07-07 17:02:07 +0530

    added favicon, using url tag for href

--- personal/urls.py
+++ personal/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
 from . import views
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/personal/img/favicon.ico/', permanent=True)
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('favicon.ico/', favicon_view, name='favicon'),
     path('contact/',views.contact,name='contact'),
     path('about/',views.about,name='about'),
 ]
