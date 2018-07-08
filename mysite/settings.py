361a57d828b5cfc4d71ed7d627cee35c9fdade54
jabir15 <jabir.hussain.aec@gmail.com>
2018-07-08 07:08:43 +0530

    changed debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q#*%j^h22xfx0zt=c7xw+35zcv4nw5e(1pec#d*u$g_h4((hyh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jabirhussain.pythonanywhere.com']
 
