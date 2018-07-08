8be33cf0f98ecdcd2f7dcd94558fac05e5ba8bda
jabirhussain <chisti.katja@gmail.com>
2018-07-08 01:02:01 +0000

    using python anywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q#*%j^h22xfx0zt=c7xw+35zcv4nw5e(1pec#d*u$g_h4((hyh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jabirhussain.pythonanywhere.com']
 
 
 # Application definition
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/jabirhussain/acc-try/static/'
