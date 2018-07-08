a7da693072040fedc687ab068c515f35b92f7802
jabir15 <jabir.hussain.aec@gmail.com>
2018-07-08 14:26:29 +0530

    favicon.ico path error

--- personal/urls.py
+++ personal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 from django.views.generic.base import RedirectView
 
-favicon_view = RedirectView.as_view(url='/static/personal/img/favicon.ico/', permanent=True)
+favicon_view = RedirectView.as_view(url='/static/personal/img/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('',views.index, name='index'),
