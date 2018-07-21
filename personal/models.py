22ac830397e84c10f992984150407bc26a0e145c
jabir15 <jabir.hussain.aec@gmail.com>
2018-07-21 17:10:34 +0530

    accept blank url

--- personal/models.py
+++ personal/models.py
@@ -6,7 +6,7 @@ class College(models.Model):
     full_address = models.CharField(max_length=200)
     college_district = models.CharField(max_length=140)
     college_pincode = models.PositiveIntegerField()
-    college_url = models.URLField()
+    college_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
