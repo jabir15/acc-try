8c852c76b144e2fa4f81e3d2532584e7967b28eb
jabir15 <jabir.hussain.aec@gmail.com>
2019-02-22 23:52:17 +0530

    AWS Trial 4

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,7 +157,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'acc-try/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
